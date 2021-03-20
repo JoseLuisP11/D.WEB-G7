ce13e8217268ab7e5942d61b13e528e3cb2b9520
JoseLuisP11 <JoseLuis11Puebla@gmail.com>
2021-03-20 11:19:45 +0100

    Creación de tests

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -12,4 +12,12 @@ class NotesController < ApplicationController
   def new
     @note = Note.new
   end
+
+
+private
+  # Only allow a list of trusted parameters through.
+  def note_params
+    params.require(:note).permit(:title, :description)
+  end
+
 end
--- spec/models/notes_spec.rb
+++ spec/models/notes_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 require './app/models/note'
 
-
-
-    describe Note do
-      describe "#new" do	    
-	    it "should create a note" do
-            note=Note.new(title:"Note Title",description:"Note description")
-            expect(note.title).to eq("Note Title")
-        end
-	    end
+describe Note do
+  context '#new' do
+    it 'should create a note' do
+      note = Note.new(title: 'Note Title', description: 'Note description')
+      expect(note.title).to eq('Note Title')
     end
+  end
 
+  context '#create' do
+    it 'should create a note' do
+      note = Note.create(title: 'Note Title', description: 'Note description')
+      expect(note.title).to eq('Note Title')
+    end
+  end
 
   
+end
