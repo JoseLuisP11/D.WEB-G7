dfd2daf7f88b9d3ce2570e7cf78afaa2a1408c14
JoseLuisP11 <JoseLuis11Puebla@gmail.com>
2021-04-19 22:48:10 +0200

    Devise User

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,4 @@
-require './app/models/application_record'
-
 class Note < ApplicationRecord
+    belongs_to :user
     has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :notes
+
+  
+  get 'notes/index'
+  get 'notes/edit'
+  get 'notes/show'
   get 'home/index'
- resources :notes
-
- 
- get 'notes/index'
- get 'notes/edit'
- get 'notes/show'
-
- 
- root 'home#index'
+  
+  root 'notes#index'
 end
