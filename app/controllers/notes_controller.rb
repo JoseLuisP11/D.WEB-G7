7c5bc6a5fe7b1182f24e759a6b3f56210d8ae063
JoseLuisP11 <JoseLuis11Puebla@gmail.com>
2021-04-24 14:51:18 +0200

    Controlador notas

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,45 +1,63 @@
 class NotesController < ApplicationController
   before_action :set_note, only: %i[ show edit update destroy ]
   
-  def index
-    @notes = Note.all 
-  end
+ # GET /notes or /notes.json
+ def index
+  @notes = Note.all
+end
 
-  def edit; end
+# GET /notes/1 or /notes/1.json
+def show
+end
 
-  def show; end
+# GET /notes/new
+def new
+  @note = Note.new
+  #@note = current_user.notes.build()
+end
 
-  # GET /notes/new
-  def new
-    @note = Note.new
-  end
+# GET /notes/1/edit
+def edit
+end
 
-  # POST /notes or /notes.json
-  def create
-    @note = Note.new(note_params)
+# POST /notes or /notes.json
+def create
+  @note = Note.new(note_params)
+  #@note = current_user.notes.build(note_params)
+
+  respond_to do |format|
     if @note.save
-      redirect_to @note
+      format.html { redirect_to @note, notice: "Note was successfully created." }
+      format.json { render :show, status: :created, location: @note }
     else
-      render :new
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @note.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  # PATCH/PUT /notes/1 or /notes/1.json
-  def update
+# PATCH/PUT /notes/1 or /notes/1.json
+def update
+  respond_to do |format|
     if @note.update(note_params)
-      redirect_to @note
+      format.html { redirect_to @note, notice: "Note was successfully updated." }
+      format.json { render :show, status: :ok, location: @note }
     else
-      render :edit
+      format.html { render :edit, status: :unprocessable_entity }
+      format.json { render json: @note.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  # DELETE /notes/1 or /notes/1.json
-  def destroy
-    @note = Note.find(params[:id])
-    @note.destroy
-
-    redirect_to @note
+# DELETE /notes/1 or /notes/1.json
+def destroy
+  @note.destroy
+  respond_to do |format|
+    format.html { redirect_to notes_url, notice: "Note was successfully destroyed." }
+    format.json { head :no_content }
   end
+end
+
 
 private
   # Use callbacks to share common setup or constraints between actions.
