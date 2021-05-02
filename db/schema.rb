1b1b81ebb176378458ece05873b7d8f2a193bad2
JoseLuisP11 <JoseLuis11Puebla@gmail.com>
2021-05-02 17:38:08 +0200

    Progreso usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_211826) do
+ActiveRecord::Schema.define(version: 2021_05_02_153653) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_211826) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["title"], name: "index_collections_on_title", unique: true
   end
 
@@ -62,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_211826) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
