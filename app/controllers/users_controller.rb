48eb93e4c45ad0d502cbd99ea1e25127c04f6f77
JoseLuisP11 <JoseLuis11Puebla@gmail.com>
2021-05-05 22:49:03 +0200

    Autenticación

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create]
   before_action :set_user, only: %i[ show edit update destroy ]
+  
 
   # GET /users or /users.json
   def index
@@ -62,7 +64,7 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-
+    
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation, :admin)
